4da272a12830db1df47d6bb5e7dfcd536dd24551
ali2abdullah <ali561209@gmail.com>
2023-05-25 11:48:25 +0300

    a program that's calculate the volume of a sphere

---
+++ calculatethevolume/2.3.cpp
@@ -0,0 +1,19 @@
+//Ali Abdullah Al Omani
+//431900303
+
+#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double volume, radius, pi;
+    radius = 5;
+    pi = 3.1416;
+    
+    volume = (4.0/3) * pi * (pow(radius,3));
+    cout << " The Volume is = " << volume;
+    
+    return 0;
+}
+
